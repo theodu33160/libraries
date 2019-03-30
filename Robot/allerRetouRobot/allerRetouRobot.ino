4bc99e8852866d504a4f1d0e61a010c24d99f162
unknown <theo.degeorges@gmail.com>
2019-03-30 16:28:59 +0100

    mise à jour de l'exemple

--- Robot/allerRetouRobot/allerRetouRobot.ino
+++ Robot/allerRetouRobot/allerRetouRobot.ino
@@ -1,9 +1,12 @@
+#include <SoftwareSerial.h>
+
+
+
 //Type de carte de puissace pour les moteurs :
 #define PONT_EN_H  //  PONT_EN_H - MD25
 
 //Type de moyen de repérage du robot
 #define CODEURS   // CODEURS    -   SOURIS    -   EXTERNE
-
 
 
 byte ledON = 26;
@@ -17,11 +20,7 @@ byte ledEtape3 = 37;
 byte ledEtape4 = 38;
 
 
-
-
-
 #include <Robot.h>
-#include <DCMotor.h>
 byte codeurGauche1 = 20; // pour interruptPin seulement les pin 2, 3, 18, 19, 20 , 21 fonctionnent
 byte codeurGauche2 = 21; // pour interruptPin seulement les pin 2, 3, 18, 19, 20, 21 fonctionnent
 byte codeurDroite1 = 19; // pour interruptPin seulement les pin 2, 3, 18, 19, 20, 21 fonctionnent
@@ -31,7 +30,7 @@ byte codeurDroite2 = 18; // pour interruptPin seulement les pin 2, 3, 18, 19, 20
 DCMotor moteurDroit(2, 4, 3, codeurDroite1 , codeurDroite2);  //PWM, IN1, IN2, VOIEA, VOIEB
 DCMotor moteurGauche(7,6,5, codeurGauche1 , codeurGauche2); //PWM, IN1, IN2, VOIEA, VOIEB
 
-Robot monRobot = Robot(&moteurGauche,&moteurDroit, 207.5/2, 40); //roue gauche, roue droite, empattement, rayon roue
+Robot monRobot = Robot(&moteurGauche,&moteurDroit, 207.5/2, 39.4); //roue gauche, roue droite, empattement = 207.5, rayon roue = 39.4
 
 
 void setup() 
@@ -44,7 +43,7 @@ void setup()
 //  Serial.println(monRobot.getBattery());
 */
   moteurDroit.setPID(1.429,-1.346,0);
-  moteurGauche.setPID(0.6452,-0.5907,0);
+  moteurGauche.setPID(0.9013, -0.826,0);
 /*  pinMode(codeurGauche1,INPUT);
   pinMode(codeurGauche2,INPUT);
   pinMode(codeurDroite1,INPUT);
@@ -54,9 +53,15 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(codeurGauche2), motorCodeurIncrementalGB, CHANGE);
   attachInterrupt(digitalPinToInterrupt(codeurDroite1), motorCodeurIncrementalDA, CHANGE);
   attachInterrupt(digitalPinToInterrupt(codeurDroite2), motorCodeurIncrementalDB, CHANGE);
+  monRobot.setAngleCorrecteur(0.3,0.4,0); //(0.55,0.8,0)
+  monRobot.setDistanceCorrecteur(0.05,0.2,0); //(0.1,0.2,0);
+  monRobot.setAngleSvrPtCorrecteur(1,0.25,0); //(0.25,0.25,0);
+  monRobot.setIntegralSaturation(2);
+  monRobot.setRapportAvancerTourner(0);
   monRobot.initLeds();
   //delay(2000);
   //monRobot.jeuLeds();
+  //monRobot.setPosition(300,0,180);
 }
 
 void loop() 
@@ -64,13 +69,11 @@ void loop()
   //monRobot.reglagePinceManuel();
   monRobot.tirageTirette();
   monRobot.actualiserPosition();
-  monRobot.suivrePoint(100,0,5);
-  //moteurDroit.tourneRPM(10);
-  //moteurGauche.tourneRPM(10);
-  //monRobot.avancerTourner(10,0);
-  //Serial.println(moteurGauche.getCodeur());
-  monRobot.debug();
-  
+  //monRobot.allerRetour(500,0);
+  //monRobot.avancer(10,10);
+  //monRobot.suivrePoint(2500,0,1);
+  monRobot.tournerPrecis(180,0.2);
+  monRobot.debug();  
 }
 
 void motorCodeurIncrementalDA()
