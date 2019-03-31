3bc56093f6ad7ccee319706524edb8822024b178
Raphaël Gerin <raphael.gerin@gmail.com>
2019-03-31 23:08:08 +0200

    sauvegarde du code exemple de la calsse robot

--- Robot/allerRetouRobot/allerRetouRobot.ino
+++ Robot/allerRetouRobot/allerRetouRobot.ino
@@ -1,4 +1,3 @@
-
 //Type de carte de puissace pour les moteurs :
 #define PONT_EN_H  //  PONT_EN_H - MD25
 
@@ -16,9 +15,8 @@ byte ledEtape2 = 36;
 byte ledEtape3 = 37;
 byte ledEtape4 = 38;
 
-#include <Robot.h>
-#include <DCMotor.h>
 
+#include <Robot.h>
 byte codeurGauche1 = 20; // pour interruptPin seulement les pin 2, 3, 18, 19, 20 , 21 fonctionnent
 byte codeurGauche2 = 21; // pour interruptPin seulement les pin 2, 3, 18, 19, 20, 21 fonctionnent
 byte codeurDroite1 = 19; // pour interruptPin seulement les pin 2, 3, 18, 19, 20, 21 fonctionnent
@@ -28,32 +26,38 @@ byte codeurDroite2 = 18; // pour interruptPin seulement les pin 2, 3, 18, 19, 20
 DCMotor moteurDroit(2, 4, 3, codeurDroite1 , codeurDroite2);  //PWM, IN1, IN2, VOIEA, VOIEB
 DCMotor moteurGauche(7,6,5, codeurGauche1 , codeurGauche2); //PWM, IN1, IN2, VOIEA, VOIEB
 
+Robot monRobot = Robot(&moteurGauche,&moteurDroit, 207.5/2, 40); //roue gauche, roue droite, empattement, rayon roue
 
-Robot monRobot = Robot(&moteurGauche,&moteurDroit, 207.5/2, 39.4); //roue gauche, roue droite, empattement = 207.5, rayon roue = 39.4
 
 void setup() 
 { 
   TCCR3B = TCCR3B & 0b11111000 | 0x01;  //permet d'augmenter la fréquence du pwm des pins 5,3 et 2 au maximum (31 250Hz)
   TCCR4B = TCCR4B & 0b11111000 | 0x01;  //permet d'augmenter la fréquence du pwm des pins 8,7 et 6 au maximum (31 250Hz)
   
+/*  Serial.begin(115200);
+  Serial.println("###################pute##################################");
+//  Serial.println(monRobot.getBattery());
+*/
   moteurDroit.setPID(1.429,-1.346,0);
-  moteurGauche.setPID(0.9013, -0.826,0);
-
+  moteurGauche.setPID(0.6452,-0.5907,0);
+/*  pinMode(codeurGauche1,INPUT);
+  pinMode(codeurGauche2,INPUT);
+  pinMode(codeurDroite1,INPUT);
+  pinMode(codeurDroite2,INPUT);
+  */
   attachInterrupt(digitalPinToInterrupt(codeurGauche1), motorCodeurIncrementalGA, CHANGE);
   attachInterrupt(digitalPinToInterrupt(codeurGauche2), motorCodeurIncrementalGB, CHANGE);
   attachInterrupt(digitalPinToInterrupt(codeurDroite1), motorCodeurIncrementalDA, CHANGE);
   attachInterrupt(digitalPinToInterrupt(codeurDroite2), motorCodeurIncrementalDB, CHANGE);
-
-  monRobot.setAngleCorrecteur(0.3,0.4,0); //(0.55,0.8,0)
-  monRobot.setDistanceCorrecteur(0.05,0.2,0); //(0.1,0.2,0);
-  monRobot.setAngleSvrPtCorrecteur(1,0.25,0); //(0.25,0.25,0);
+  monRobot.setAngleCorrecteur(0.55,0.8,0);
+  monRobot.setDistanceCorrecteur(0.1,0.2,0);
+  monRobot.setAngleSvrPtCorrecteur(0.005,0.005,0);
+  
   monRobot.setIntegralSaturation(2);
-  monRobot.setRapportAvancerTourner(0);
+  smonRobot.setRapportAvancerTourner(0);
   monRobot.initLeds();
   //delay(2000);
   //monRobot.jeuLeds();
-  //monRobot.setPosition(300,0,180);
-
 }
 
 void loop() 
@@ -61,13 +65,11 @@ void loop()
   //monRobot.reglagePinceManuel();
   monRobot.tirageTirette();
   monRobot.actualiserPosition();
-
   //monRobot.allerRetour(500,0);
-  //monRobot.avancer(10,10);
-  //monRobot.suivrePoint(2500,0,1);
-  monRobot.tournerPrecis(180,0.2);
-  monRobot.debug();  
-
+  monRobot.avancer(-10,-10);
+  //monRobot.suivrePoint(300,0,2);
+  //monRobot.tournerPrecis(-45,0.2);
+  monRobot.debug();
 }
 
 void motorCodeurIncrementalDA()
